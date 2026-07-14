f4c726be096460ad14f48ee28aa6a51fe9cef0a9
Goutham <goutham24.2006@gmail.com>
2026-07-14 16:49:45 +0530

    Support dynamic PORT for deployment

--- cmd/gosearch/main.go
+++ cmd/gosearch/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"gosearch/api"
 	"gosearch/storage"
@@ -22,6 +23,11 @@ func main() {
 	http.HandleFunc("/search", api.EnableCORS(server.SearchHandler))
 	http.HandleFunc("/crawl", api.EnableCORS(server.CrawlHandler))
 
-	fmt.Println("GoSearch API running at http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	fmt.Println("GoSearch API running on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
